0e04f8f5e00921a4ffa356b194154de85fe3e20e
biruk90-arch <biruktakele90@gmail.com>
2023-03-16 23:27:03 +0000

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * main - puts*
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(voidi);
 {
-puts("\"Programming is like building a multilingual puzzle")
+puts("Programming is like building a multilingual puzzle")
 return (0);
 }
