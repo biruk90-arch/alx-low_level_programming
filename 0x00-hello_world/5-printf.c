053878e77adcac19b423f0ca665b95fb22744c02
biruk90-arch <biruktakele90@gmail.com>
2023-03-16 23:53:14 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 /*
  * main -prinf
  *Return: Always 0 (Success)
  */
-int main(void);
+int main(void)
+
 {
 printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
