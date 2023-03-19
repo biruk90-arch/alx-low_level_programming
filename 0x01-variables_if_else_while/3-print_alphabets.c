d07d9aa9bbf8df85b8b9eae29d54129c4eda29ba
biruk90-arch <biruktakele90@gmail.com>
2023-03-19 22:41:17 +0000

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char b;
+c = 'a';
+b = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(b <= 'Z') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+return (0);
+}
